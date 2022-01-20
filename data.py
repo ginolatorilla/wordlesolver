d04796a1abf1a9bf0c74c7bf23e689a2113d71a6
Gino Latorilla <pblatorilla+github@gmail.com>
2022-01-20 18:31:40 +0800

    Log why word is not OK for Wordle

--- data.py
+++ data.py
@@ -17,6 +17,8 @@ def read_wordle_dictionary() -> Iterator[str]:
     for word in read_wamerican():
         if len(word) == 5 and word.isalpha() and word.islower():
             yield word
+        else:
+            logging.debug(f'"{word}" is not a suitable word for Wordle, so I will drop it.')
 
 
 _PATH_TO_AMERICAN_ENGLISH_DICTIONARY = '/usr/share/dict/american-english'
