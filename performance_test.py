3f00ae63988ef54d832f7115ca26edcac914b97f
Gino Latorilla <pblatorilla+github@gmail.com>
2022-01-26 14:02:07 +0800

    Partial calibration test

---
+++ performance_test.py
@@ -0,0 +1,28 @@
+from cProfile import Profile
+
+import analytics
+
+
+def test_Predictor_init_performance() -> None:
+    with Profile() as profiler:
+        analytics.Predictor()
+
+    profiler.print_stats(sort='time')
+
+
+def test_Predictor_predict_wordle_1st_round_performance() -> None:
+    predictor = analytics.Predictor()
+
+    with Profile() as profiler:
+        predictor.predict_wordle()
+
+    profiler.print_stats(sort='time')
+
+
+def test_Predictor_calibrate_performance() -> None:
+    predictor = analytics.Predictor()
+
+    with Profile() as profiler:
+        predictor.calibrate('crate', 'mmmmm')
+
+    profiler.print_stats(sort='time')
